ad662d6c7c13ac63bd8c9176dc9f2b3d2e78ab0e
rrzq <rzq272@gmail.com>
2022-08-11 02:05:27 +0800

    new file:   .prettierignore 	new file:   database/migrations/2022_08_10_145429_create_categories_table.php 	new file:   public/assets/favicon.ico 	new file:   public/css/styles.css 	new file:   public/js/scripts.js 	new file:   resources/views/about.blade.php 	new file:   resources/views/contact.blade.php 	new file:   resources/views/index.blade.php 	new file:   resources/views/layouts/app.blade.php 	deleted:    resources/views/welcome.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', view: 'index')->name(name: 'home');
+Route::view('/about', view: 'about')->name(name: 'about');
+Route::view('/contact', view: 'contact')->name(name: 'contact');
